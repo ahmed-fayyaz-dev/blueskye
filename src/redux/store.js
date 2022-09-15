694d8b0bebbb8cd750282616787c9fc273cfd022
anasnaeem135 <anasnaeem135@gmail.com>
2022-09-15 19:23:55 +0500

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,25 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { composeWithDevTools } from "@redux-devtools/extension";
-import { createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
-import thunk from "redux-thunk";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 
-import { saveOnLogin } from "./middleware/otherMiddleWare";
-import { ServiceMiddleware } from "./middleware/serviceMiddleWare.js";
-import rootReducers from "./reducer";
+import { saveOnLogin } from './middleware/otherMiddleWare';
+import { ServiceMiddleware } from './middleware/serviceMiddleWare.js';
+import rootReducers from './reducer';
 
 const persistConfig = {
-    key: "root",
+    key: 'root',
     storage: AsyncStorage,
-    whitelist: ["loginUserReducer"],
+    whitelist: ['loginUserReducer'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducers);
 const middleware = [thunk, ServiceMiddleware, saveOnLogin];
 const store = createStore(
     persistedReducer,
-    composeWithDevTools(applyMiddleware(...middleware))
+    composeWithDevTools(applyMiddleware(...middleware)),
 );
 let persistor = persistStore(store);
 
-export { store, persistor };
+export { store };
