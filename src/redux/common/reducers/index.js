31316f8718cc51b6baac1bce920489f6f77570f7
ahmed <61820697+ahmed-fayyaz-dev@users.noreply.github.com>
2022-09-15 17:02:11 +0500

    redux migrated to reduxToolkit

--- src/redux/common/reducers/index.js
+++ src/redux/common/reducers/index.js
@@ -1,14 +1,5 @@
-import addressReducer from './addressReducer';
-import gDateReducer from './globalDateReducer';
 import languageReducer from './languageReducer';
-import locationReducer from './locationReducer';
-import appAppearanceReducer from './setAppApearanceReducer';
-
 const reducers = {
-    appAppearanceReducer,
-    // addressReducer,
-    // locationReducer,
     languageReducer,
-    // gDateReducer,
 };
 export default reducers;
