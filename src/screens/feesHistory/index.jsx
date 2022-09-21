9bb8104bc3550c1be7fb57d8aef66598d9adc85f
anasnaeem135 <anasnaeem135@gmail.com>
2022-09-21 16:00:13 +0500

    fee history

--- src/screens/feesHistory/index.jsx
+++ src/screens/feesHistory/index.jsx
@@ -1,29 +1,40 @@
-import React, { useState } from 'react';
-import { ScrollView, View, StyleSheet, Image, Text } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import AppBar from 'src/components/appbar';
-import { icons } from 'assets/images';
-import { CustomRoundButton } from 'src/components/buttons';
-import { CustomSubheading } from 'src/components/customText';
 import { GapV } from 'src/components/gap';
-import globalStyles, { bRss, mgM, mgMs, pdH } from 'src/styles/index';
+import globalStyles, {
+    bRss,
+    mgM,
+    mgMs,
+    mgS,
+    mgSs,
+    pdH,
+    pdHm,
+    pdHs,
+} from 'src/styles/index';
+import ListItem from './helpers/renderIList';
+import { arr } from './dummyData';
 
 const FeesHistory = ({ navigation }) => {
-    const [onPress, setOnPress] = useState(false);
     const { colors } = useTheme();
     const style = styles(colors);
     const title = 'Fees History';
 
+    const ItemSeparator = () => <GapV />;
+
     return (
         <View style={[style.container]}>
             <AppBar navigation={navigation} title={title} />
-            {/* <ScrollView contentContainerStyle={style.content}> */}
-            {/* {TopView()} */}
-            {/* {QR()} */}
-            {/* {BottomView()} */}
 
+            <FlatList
+                data={arr}
+                keyExtractor={item => item.date}
+                ItemSeparatorComponent={ItemSeparator}
+                renderItem={ListItem}
+                contentContainerStyle={style.content}
+            />
             <GapV />
-            {/* </ScrollView> */}
         </View>
     );
 };
@@ -36,44 +47,9 @@ const styles = colors =>
             flex: 1,
         },
 
-        card: {
-            borderRadius: bRss,
-            paddingTop: mgMs,
-            paddingHorizontal: pdH,
-        },
-
         content: {
             flexGrow: 1,
-            paddingHorizontal: pdH,
+            paddingHorizontal: pdHs,
             paddingTop: mgM,
         },
-
-        image: {
-            alignSelf: 'center',
-            height: 60,
-            width: 60,
-        },
-        camera: {
-            paddingTop: pdH,
-        },
-
-        qr: {
-            alignSelf: 'center',
-            height: 200,
-            width: 200,
-        },
-
-        fdr: { flexDirection: 'row' },
-
-        subText: {
-            color: colors.light,
-            fontSize: 20,
-        },
-
-        button: {
-            backgroundColor: colors.primary,
-            alignSelf: 'center',
-        },
-
-        bottomView: { justifyContent: 'center' },
     });
