c5aa8e48f4755161748741938eb0eebbcae1bf75
ahmed <61820697+ahmed-fayyaz-dev@users.noreply.github.com>
2022-11-21 21:36:26 +0500

    fcm - andorid

--- App.jsx
+++ App.jsx
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
 import 'expo-dev-client';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { StyleSheet } from 'react-native';
+import * as Notifications from 'expo-notifications';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import {
@@ -12,12 +13,47 @@ import { Provider as StoreProvider } from 'react-redux';
 
 import { settings } from 'src/settings';
 import ErrorBoundary from 'src/components/errorBoundary';
+import {
+    NotificationSettings,
+    registerForPushNotificationsAsync,
+} from 'src/helpers/notifications.utils';
 import AppNavigator from 'src/navigator/navigation';
 import { store } from 'src/redux/store';
 
 settings;
+NotificationSettings;
 
 export default function App() {
+    const notificationListener = useRef();
+    const responseListener = useRef();
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState(false);
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token =>
+            setExpoPushToken(token),
+        );
+
+        notificationListener.current =
+            Notifications.addNotificationReceivedListener(notification => {
+                setNotification(notification);
+            });
+
+        responseListener.current =
+            Notifications.addNotificationResponseReceivedListener(response => {
+                console.log(response);
+            });
+
+        return () => {
+            Notifications.removeNotificationSubscription(
+                notificationListener.current,
+            );
+            Notifications.removeNotificationSubscription(
+                responseListener.current,
+            );
+        };
+    }, []);
+
     return (
         <GestureHandlerRootView style={styles.container}>
             <SafeAreaProvider initialMetrics={initialWindowMetrics}>
